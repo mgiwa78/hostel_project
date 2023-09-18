99b3cbf834a0d6588f1b2583268be4f4f20316d8
Giwa Muhammad <mgiwa78@gmail.com>
2023-09-18 06:04:44 +0100

    Minor fix

--- src/components/form/authInput.tsx
+++ src/components/form/authInput.tsx
@@ -19,7 +19,12 @@ const AuthInput = ({
 }: AuthInputTypes) => {
   return (
     <div
-      style={{height: '56px', backgroundColor: '#F7F8F9', border: '1px solid  #E8ECF4'}}
+      style={{
+        height: '56px',
+        maxWidth: '462px',
+        backgroundColor: '#F7F8F9',
+        border: '1px solid  #E8ECF4',
+      }}
       className='input-group input-group-solid mb-5'
     >
       <input
